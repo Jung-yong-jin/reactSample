86ef981fb867de311f2dc8edd2b36cd1478a073a
dydwls1641 <dydwls1641@gmail.com>
2021-07-30 17:25:20 +0900

    style add

--- src/components/text/Text.stories.jsx
+++ src/components/text/Text.stories.jsx
@@ -12,3 +12,39 @@ textStories.args = {
   xsmall: true,
   primary: true,
 };
+
+export const large_Sample = Template.bind({});
+large_Sample.args = {
+  xsmall: true,
+  primary: true,
+};
+
+export const xlarge_Sample = Template.bind({});
+xlarge_Sample.args = {
+  xsmall: true,
+  primary: true,
+};
+
+export const small_Sample = Template.bind({});
+small_Sample.args = {
+  xsmall: true,
+  primary: true,
+};
+
+export const xsmall_Sample = Template.bind({});
+xsmall_Sample.args = {
+  xsmall: true,
+  primary: true,
+};
+
+export const primary_Sample = Template.bind({});
+primary_Sample.args = {
+  xsmall: true,
+  primary: true,
+};
+
+export const secondary_Sample = Template.bind({});
+secondary_Sample.args = {
+  xsmall: true,
+  primary: true,
+};
